19827ef2966e64bad4466e872fcff467638ccdf4
Daniel <danxvv@gmail.com>
2019-06-12 20:15:09 -0500

    cambios 3

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:componentes/src/pages/card_page.dart';
 import 'package:flutter/material.dart';
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/routes/routes.dart';
-
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,16 +10,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        // ... app-specific localization delegate[s] here
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en'), // English
+        const Locale('es','MX'), // Hebrew
+        const Locale.fromSubtags(
+            languageCode: 'zh'), // Chinese *See Advanced Locales below*
+        // ... other locales the app supports
+      ],
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
       //home: HomePage()
       initialRoute: '/',
       routes: getApplicationRoutes(),
-      onGenerateRoute: (settings){
-        return MaterialPageRoute(
-          builder: (BuildContext context) => CardPage()
-        );
-      },
     );
   }
 }
